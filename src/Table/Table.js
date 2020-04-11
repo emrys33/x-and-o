14c753bdb4cece396e10058034c4cbec3bd42515
emrys33 <greatparsa@gmail.com>
2020-04-11 18:39:56 +0430

    first commit

---
+++ src/Table/Table.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import './Table.css';
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <tr>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                </tr>
+                <tr>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                </tr>
+                <tr>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                    <td>Hi</td>
+                </tr>
+            </table>
+        )
+    };
+};
+
+export default Table;
